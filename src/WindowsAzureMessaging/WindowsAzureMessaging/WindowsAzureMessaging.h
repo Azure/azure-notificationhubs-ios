e59699f3580c41b0b391b2753f5dfb5aa81ecdba
Matthew Podwysocki <matthewp@microsoft.com>
2020-04-25 17:22:08 -0400

    Adding UIKit explicitly

--- src/WindowsAzureMessaging/WindowsAzureMessaging/WindowsAzureMessaging.h
+++ src/WindowsAzureMessaging/WindowsAzureMessaging/WindowsAzureMessaging.h
@@ -2,5 +2,6 @@
 //  Copyright (c) Microsoft Corporation. All rights reserved.
 //----------------------------------------------------------------
 
+#import <Foundation/Foundation.h>
 #import "SBNotificationHub.h"
 #import "SBConnectionString.h"
