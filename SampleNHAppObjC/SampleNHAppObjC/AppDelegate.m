5e1d8ac15c681db84ae8fed3b40952892b612bdf
Artem Egorov <toysruss89@gmail.com>
2020-07-02 12:57:18 +0300

    Reimplement Sample App for ObjC iOS

--- SampleNHAppObjC/SampleNHAppObjC/AppDelegate.m
+++ SampleNHAppObjC/SampleNHAppObjC/AppDelegate.m
@@ -1,16 +1,10 @@
-//
-//  AppDelegate.m
-//  SampleNHAppObjC
-//
-//  Created by Matthew Podwysocki on 6/30/20.
-//  Copyright © 2020 Matthew Podwysocki. All rights reserved.
-//
+//----------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//----------------------------------------------------------------
 
 #import "AppDelegate.h"
-#import <UserNotifications/UserNotifications.h>
-#import <WindowsAzureMessaging/WindowsAzureMessaging.h>
 
-@interface AppDelegate () <MSNotificationHubDelegate>
+@interface AppDelegate ()
 
 @end
 
@@ -18,40 +12,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
+    // Override point for customization after application launch.
     NSString *path = [[NSBundle mainBundle] pathForResource:@"DevSettings" ofType:@"plist"];
     NSDictionary *configValues = [NSDictionary dictionaryWithContentsOfFile:path];
     
     NSString *connectionString = [configValues objectForKey:@"CONNECTION_STRING"];
     NSString *hubName = [configValues objectForKey:@"HUB_NAME"];
     
-    [MSNotificationHub setDelegate:self];
     [MSNotificationHub startWithConnectionString:connectionString hubName:hubName];
+    [MSNotificationHub addTag:@"userAgent:com.microsoft.SampleNHAppObjC:1.0"];
     
     return YES;
-}
-
-#pragma mark MSNotificationHubDelegate
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-  NSLog(@"Did register for remote notifications with device token.");
-}
-
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(nonnull NSError *)error {
-  NSLog(@"Did fail to register for remote notifications with error %@.", [error localizedDescription]);
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary<NSString *, id> *)userInfo {
-  NSLog(@"Did receive remote notification");
-}
-
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didActivateNotification:(UNNotification *)notification {
-  NSLog(@"Did receive user notification");
-}
-
-- (void)notificationHub:(MSNotificationHub *)notificationHub didReceivePushNotification:(MSNotificationHubMessage *)message {
-    NSLog(@"Message title: %@", message.title);
-    NSLog(@"Message body: %@", message.body);
 }
 
 
@@ -70,6 +41,5 @@
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
-
 
 @end
--- SampleNHAppObjC/SampleNHAppObjC/main.m
+++ SampleNHAppObjC/SampleNHAppObjC/main.m
@@ -1,10 +1,6 @@
-//
-//  main.m
-//  SampleNHAppObjC
-//
-//  Created by Matthew Podwysocki on 6/30/20.
-//  Copyright © 2020 Matthew Podwysocki. All rights reserved.
-//
+//----------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//----------------------------------------------------------------
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
