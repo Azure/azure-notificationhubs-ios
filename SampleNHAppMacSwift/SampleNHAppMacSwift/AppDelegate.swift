358fe2051fec0c82ec9a71b3c9dda6e65697ac81
Artem Egorov <toysruss89@gmail.com>
2020-07-02 14:29:44 +0300

    Implement Sample App for Swift macOS

--- SampleNHAppMacSwift/SampleNHAppMacSwift/AppDelegate.swift
+++ SampleNHAppMacSwift/SampleNHAppMacSwift/AppDelegate.swift
@@ -1,26 +1,43 @@
-//
-//  AppDelegate.swift
-//  SampleNHAppMacSwift
-//
-//  Created by Matthew Podwysocki on 6/30/20.
-//  Copyright © 2020 Matthew Podwysocki. All rights reserved.
-//
+//----------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//----------------------------------------------------------------
 
-import Cocoa
+import UIKit
+import WindowsAzureMessaging
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var connectionString: String?
+    var hubName: String?
 
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        if let path = Bundle.main.path(forResource: "DevSettings", ofType: "plist") {
+            if let configValues = NSDictionary(contentsOfFile: path) {
+                connectionString = configValues["CONNECTION_STRING"] as? String
+                hubName = configValues["HUB_NAME"] as? String
+            }
+        }
+        
+        MSNotificationHub.start(connectionString: connectionString!, hubName: hubName!)
+        MSNotificationHub.addTag("userAgent:com.microsoft.SampleNHAppMacSwift:1.0")
+        
+        return true
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
 
 }
-
