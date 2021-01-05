01dc7b0d3dc44e8c10d7d885588a89bd3482663e
Matthew Podwysocki <matthewp@microsoft.com>
2021-01-05 15:38:47 -0500

    Removing bundles

--- Scripts/combine-xcframework.sh
+++ Scripts/combine-xcframework.sh
@@ -18,13 +18,3 @@ done
 
 # Build XCFramework.
 xcodebuild -create-xcframework "${XC_FRAMEWORKS[@]}" -output "${BUILT_PRODUCTS_DIR}/${PROJECT_NAME}.xcframework"
-
-# Copy the resource bundle.
-BUNDLE_NAME="${PROJECT_NAME}Resources.bundle"
-BUNDLE_PATH="${BUILD_DIR}/${CONFIGURATION}-iphoneos/${BUNDLE_NAME}"
-if [ -e "${BUNDLE_PATH}" ]; then
-  rm -rf "${BUILT_PRODUCTS_DIR}/${BUNDLE_NAME}"
-  cp -Rv "${BUNDLE_PATH}" "${BUILT_PRODUCTS_DIR}"
-fi
-echo "Cleanup resource bundles inside frameworks"
-rm -rfv "${BUILT_PRODUCTS_DIR}/${PROJECT_NAME}.xcframework/*/${PRODUCT_NAME}.framework/${BUNDLE_NAME}"
