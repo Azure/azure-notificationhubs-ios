98fb324966ba02da56cf4f4559fe4afc2e26fbc0
hyounoo <hyounoosung@gmail.com>
2021-01-16 16:24:30 +0900

    Create iOS14 SwiftUI2 sample project

---
+++ SampleNHAppSwiftUI2/SampleNHAppSwiftUI2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleNHAppSwiftUI2
+//
+//  Created by Hyounwoo Sung on 2021/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleNHAppSwiftUI2/SampleNHAppSwiftUI2/SampleNHAppSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleNHAppSwiftUI2App.swift
+//  SampleNHAppSwiftUI2
+//
+//  Created by Hyounwoo Sung on 2021/01/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleNHAppSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
